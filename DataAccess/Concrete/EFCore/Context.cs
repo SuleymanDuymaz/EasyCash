e5d4a8c690442dd37d27bb71a4b9772bc1b5866d
Süleyman <sduymaz8@gmail.com>
2023-05-01 16:46:05 +0300

    database created

---
+++ DataAccess/Concrete/EFCore/Context.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAccessLayer.Concrete.EFCore
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-9L38DMG;database=EasyCash;integrated security=true;");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+
+    }
+}
---
+++ Entity/Concrete/CustomerAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class CustomerAccount
+    {   
+        [Key]
+        public int CustomerAccountID { get; set; }
+        public string CustomerAccountNumber { get; set; }
+        public string CustomerAccountCurrency { get; set; }
+        public decimal CustomerAccountBalance { get; set; }
+        public string BankBranch { get; set; }
+    }
+}
