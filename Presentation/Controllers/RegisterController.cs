4fbbff2507dc4c1bdec2a21bef023f01ef08e4cb
Süleyman <sduymaz8@gmail.com>
2023-05-21 22:10:30 +0300

    controller

--- Presentation/Controllers/RegisterController.cs
+++ Presentation/Controllers/RegisterController.cs
@@ -1,17 +1,37 @@
 ﻿using DTO.DTOS.AppUserDto;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult Index(AppUserRegisterDto appUserRegisterDto)
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
         {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser()
+                {
+                    UserName = appUserRegisterDto.UserName,
+                    Name = appUserRegisterDto.Name,
+                    Surname=appUserRegisterDto.SurName,
+                    Email = appUserRegisterDto.Email,
 
+                };
+                var result=await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index","ConfirmMail");
+                }
+            }
             return View();
         }
     }
