40329742a5ba58d8dd916a56ce9241d19d94491d
Süleyman <sduymaz8@gmail.com>
2023-06-04 16:28:10 +0300

    confirm

--- Presentation/Controllers/ConfirmMailController.cs
+++ Presentation/Controllers/ConfirmMailController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Models;
 
 namespace Presentation.Controllers
 {
 	public class ConfirmMailController : Controller
 	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+        private readonly UserManager<AppUser> _userManager;
+        public ConfirmMailController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var value = TempData["Mail"];
+            ViewBag.v = value;
+            //  confirmMailViewModel.Mail = value.ToString();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ConfirmMailViewModel confirmMailViewModel)
+        {
+            var user = await _userManager.FindByEmailAsync(confirmMailViewModel.Mail);
+            if (user.ConfirmCode == confirmMailViewModel.ConfirmCode)
+            {
+                user.EmailConfirmed = true;
+                await _userManager.UpdateAsync(user);
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
 }
---
+++ Presentation/Models/ConfirmMailViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Presentation.Models
+{
+    public class ConfirmMailViewModel
+    {
+        public string Mail { get; set; }
+        public int ConfirmCode { get; set; }
+    }
+}
